a84a3486a12a643e6da6873ea425d69a74dc3927
Shrey <shreymishra0307@gmail.com>
2024-03-09 23:43:23 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,24 @@ const app = express();
 // Middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // Route handlers for the mentor and student routes
 app.use("/mentors", mentorRoutes);
 app.use("/students", studentRoutes);
